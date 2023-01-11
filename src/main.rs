6f54c49f7db0a61ad4963d20f89ad7e2919ee54a
Ed Jones <ed.jones@canonical.com>
2023-01-11 23:52:24 +0000

    Output an example when ‘init’ is passed

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,40 @@ mod spoof;
 use args::Args;
 use clap::Parser;
 use spoof::Spoof;
+use std::env;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+const EXAMPLE: &str = r"
+- cmd: gcc --version
+- print:
+  - gcc (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0
+  - Copyright (C) 2021 Free Software Foundation, Inc.
+  - This is free software; see the source for copying conditions.  There is NO
+  - warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+";
 
 fn main() {
     let args = Args::parse();
     let fname = args.input();
 
+    if fname == "init" {
+        init_example();
+        return;
+    }
+
     let spoof = match Spoof::from_file(fname) {
         Ok(s) => s,
         Err(e) => {
-            eprintln!("failed to open file {}: {}", fname, e);
+            if fname == "cli.yml" {
+                eprintln!(
+                    "no such file '{}'\nrun `{} init` to create an example file",
+                    fname,
+                    env::args().next().unwrap()
+                );
+            } else {
+                eprintln!("failed to open file '{}': {}", fname, e);
+            }
             return;
         }
     };
@@ -35,4 +60,10 @@ fn main() {
 
     ncurses::delwin(window);
     ncurses::endwin();
+}
+
+fn init_example() {
+    let f = File::create("cli.yml").unwrap();
+    let mut w = BufWriter::new(f);
+    write!(w, "{}", &EXAMPLE[1..]).unwrap();
 }
