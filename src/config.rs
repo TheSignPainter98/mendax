0fc1d78849338ac936726fa01a5c9a1abb5a9396
Ed Jones <ed.jones@canonical.com>
2023-03-03 16:34:28 +0000

    Test rhai interface

--- src/config.rs
+++ src/config.rs
@@ -3,13 +3,13 @@ use lazy_static::lazy_static;
 use rhai::{Array, CustomType, Engine, EvalAltResult, Map, Scope, TypeBuilder};
 use thiserror::Error;
 
-pub fn read(fname: &str, unrestricted: bool) -> Result<Lie, Box<EvalAltResult>> {
+pub fn read<T: AsRef<str>>(fname: T, unrestricted: bool) -> Result<Lie, Box<EvalAltResult>> {
     let engine = engine(unrestricted);
 
     let mut scope = Scope::new();
     scope.push("lie", Lie::new(unrestricted));
 
-    engine.run_file_with_scope(&mut scope, fname.into())?;
+    engine.run_file_with_scope(&mut scope, fname.as_ref().into())?;
 
     Ok(scope.get_value("lie").unwrap())
 }
@@ -91,11 +91,7 @@ impl Lie {
 
     fn system(&mut self, cmd: &str, apparent_cmd: &str) -> Result<(), Box<EvalAltResult>> {
         if !self.allow_system {
-            let problem = MendaxError::SystemForbidden;
-            return Err(Box::new(EvalAltResult::ErrorSystem(
-                        problem.to_string(),
-                        problem.into(),
-            )));
+            return Err(Box::new(MendaxError::SystemForbidden.into()));
         }
 
         let apparent_cmd = apparent_cmd.into();
@@ -130,8 +126,8 @@ impl Lie {
                         expected: vec!["speed", "fg", "bg"],
                     };
                     return Err(Box::new(EvalAltResult::ErrorSystem(
-                                err.to_string(),
-                                Box::new(err),
+                        err.to_string(),
+                        Box::new(err),
                     )));
                 }
             }
@@ -204,11 +200,11 @@ pub enum MendaxError {
 
 impl From<MendaxError> for EvalAltResult {
     fn from(value: MendaxError) -> Self {
-        EvalAltResult::ErrorSystem(value.to_string(), Box::new(value))
+        EvalAltResult::ErrorSystem("mendax error".into(), Box::new(value))
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Fib {
     Run {
         cmd: String,
@@ -224,13 +220,13 @@ pub enum Fib {
         bg: Option<Colour>,
         title: Option<String>,
         cwd: Option<String>,
-        host: Option<String>,
         user: Option<String>,
+        host: Option<String>,
     },
     Clear,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Colour {
     Red,
     Black,
@@ -255,8 +251,150 @@ impl TryFrom<&str> for Colour {
             Ok(*c)
         } else {
             Err(Box::new(
-                    MendaxError::UnknownColour(s.to_owned().into(), &COLOUR_NAMES).into(),
+                MendaxError::UnknownColour(s.to_owned().into(), &COLOUR_NAMES).into(),
             ))
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::{error::Error, fs};
+
+    fn test_script(unrestricted: bool, script: &str) -> Result<Lie, Box<dyn Error>> {
+        let dir = tempfile::tempdir()?;
+        let lie_path = &dir.path().join("test-lie.rhai");
+
+        fs::write(lie_path, script)?;
+
+        read(lie_path.as_os_str().to_str().unwrap(), unrestricted).map_err(|e| e.into())
+    }
+
+    #[test]
+    fn run() -> Result<(), Box<dyn Error>> {
+        let lie = test_script(
+            false,
+            r#"
+                lie.run("foo");
+                lie.run("bar", "qwer");
+                lie.run("baz", ["asdf", "fdsa"]);
+            "#,
+        )?;
+
+        assert_eq!(
+            lie.tale().fibs(),
+            &[
+                Fib::Run {
+                    cmd: "foo".into(),
+                    result: vec![]
+                },
+                Fib::Run {
+                    cmd: "bar".into(),
+                    result: vec!["qwer".into()]
+                },
+                Fib::Run {
+                    cmd: "baz".into(),
+                    result: vec!["asdf".into(), "fdsa".into()]
+                },
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn system() -> Result<(), Box<dyn Error>> {
+        {
+            let lie = test_script(
+                true,
+                r#"
+                    lie.system("ls");
+                    lie.system("ls -Al", "la");
+                "#,
+            )?;
+
+            assert_eq!(
+                lie.tale().fibs(),
+                &[
+                    Fib::System {
+                        cmd: "ls".into(),
+                        apparent_cmd: "ls".into(),
+                    },
+                    Fib::System {
+                        cmd: "ls -Al".into(),
+                        apparent_cmd: "la".into(),
+                    },
+                ]
+            );
+        }
+
+        {
+            match test_script(false, r#"lie.system("foo");"#) {
+                Err(e) => assert_eq!(
+                    e.to_string(),
+                    "mendax error: system commands are forbidden at this sandbox level"
+                ),
+                _ => assert!(false, "system was allowed"),
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn look() -> Result<(), Box<dyn Error>> {
+        assert_eq!(
+            test_script(false, r#"lie.look(#{});"#)?.tale().fibs(),
+            &[Fib::Look {
+                speed: None,
+                fg: None,
+                bg: None,
+                title: None,
+                cwd: None,
+                host: None,
+                user: None,
+            }],
+        );
+
+        assert_eq!(
+            test_script(
+                false,
+                r#"
+                    lie.look(#{
+                        speed: 100.0,
+                        fg: "black",
+                        bg: "red",
+                        title: "on the origin of electric toasters",
+                        cwd: "~/toast",
+                        user: "methos",
+                        host: "gaia",
+                    });
+                "#
+            )?
+            .tale()
+            .fibs(),
+            &[Fib::Look {
+                speed: Some(100.0),
+                fg: Some(Colour::Black),
+                bg: Some(Colour::Red),
+                title: Some("on the origin of electric toasters".into()),
+                cwd: Some("~/toast".into()),
+                host: Some("gaia".into()),
+                user: Some("methos".into()),
+            }]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn clear() -> Result<(), Box<dyn Error>> {
+        assert_eq!(
+            test_script(false, r#"lie.clear()"#)?.tale().fibs(),
+            &[Fib::Clear]
+        );
+
+        Ok(())
     }
 }
